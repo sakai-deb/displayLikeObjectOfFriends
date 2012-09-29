59f8d2f0a416d16e3295b7a33596e2d140befd53
Okawa Yuki <okawa.yuki@nifty.co.jp>
2012-09-30 00:37:14 +0900

    update css

--- templates/login.php
+++ templates/login.php
@@ -7,7 +7,9 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	</head>
 	<body>
+	<div id="wrapper">
     	<h1><img src="./images/title.png" alt="DISPLAY FRIENDS NUM OF SAME LIKES OBJECT"></h1>
 		<p class="btn"><?php echo '<a href="' . $login_url . '">Login</a>'; ?></p>
+	</div>
 	</body>
 </html>
